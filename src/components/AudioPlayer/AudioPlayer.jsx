c11c35a8861b87b33d9e1e6a712d9e52110eb616
Mikhail <Maimakov.ma@gmail.com>
2024-01-19 21:11:38 +0300

    Сделал бургер, скелетон и выпадающие списки

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,7 +1,19 @@
+
 import './audioPlayer.css';
+import { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 
 
 function AudioPlayer ()  {
+  const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 3000)
+    }, [])
+
     return (
         <div className="bar">
           <div className="bar__content">
@@ -39,16 +51,41 @@ function AudioPlayer ()  {
                 <div className="player__track-play track-play">
                   <div className="track-play__contain">
                     <div className="track-play__image">
-                      <svg className="track-play__svg" alt="music">
+                      {isLoading ? (
+                              <Skeleton
+                                width={55}
+                                height={55}
+                                baseColor="#202020"
+                                highlightColor="#444"
+                              />
+                      ) : (
+                        <svg className="track-play__svg" alt="music">
                         <use xlinkHref="img/icon/sprite.svg#icon-note" />
                       </svg>
+                      )}                      
                     </div>
                     <div className="track-play__author">
-                      <a className="track-play__author-link" href="http://"
+                      {isLoading ? (
+                              <Skeleton
+                                width={90}
+                                baseColor="#202020"
+                                highlightColor="#444"
+                              />
+                      ) : (
+                        <a className="track-play__author-link" href="http://"
                         >Ты та...</a>
+                      )}                      
                     </div>
                     <div className="track-play__album">
-                      <a className="track-play__album-link" href="http://">Баста</a>
+                      {isLoading ? (
+                                <Skeleton
+                                   width={90}
+                                   baseColor="#202020"
+                                   highlightColor="#444"
+                                />
+                      ) : (
+                        <a className="track-play__album-link" href="http://">Баста</a>
+                      )}                      
                     </div>
                   </div>
 
---
+++ src/components/SearchBy/AuthorList.jsx
@@ -0,0 +1,17 @@
+import './StaleSearchBy.css'
+
+function AuthorList ()  {
+    return (
+        <ul className="popup-list author-list">
+            <li className="popup-list__item">Michael Jackson</li>
+            <li className="popup-list__item">Frank Sinatra</li>
+            <li className="popup-list__item">Calvin Harris</li>
+            <li className="popup-list__item">Zhu</li>
+            <li className="popup-list__item">Arctic Monkeys</li>
+            <li className="popup-list__item">Леонид Агутин</li>
+        </ul>
+    )
+}
+
+
+export default AuthorList;
